fcfb5d85c5465e0ac2849458f3cc72d2c1e2e2c8
martin-doychinov <40293024+martin-doychinov@users.noreply.github.com>
2019-08-04 19:36:21 +0100

    Add files via upload

---
+++ precache-manifest.1232184ea07ea106ff646d43ad2d17c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658458087f700c5d4c5686723eb08c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b9849eef0a5afdf63d",
+    "url": "/static/css/main.679ce626.chunk.css"
+  },
+  {
+    "revision": "ff1bd37dce1e2c9b9944",
+    "url": "/static/js/2.0a1e7f7e.chunk.js"
+  },
+  {
+    "revision": "b8b9849eef0a5afdf63d",
+    "url": "/static/js/main.c8d03c37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d72a72f609e0019511faaa44299bf390",
+    "url": "/static/media/arrow-secondary-6-sans-serif.d72a72f6.svg"
+  },
+  {
+    "revision": "0ba291142f7699593064b56bd46cf473",
+    "url": "/static/media/arrow-secondary-6-serif.0ba29114.svg"
+  }
+]);
